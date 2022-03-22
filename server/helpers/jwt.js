eba8c8dd298dad6f893d6fd75270c09a181c8133
abdulmajid <abdulmajid08180881@gmail.com>
2022-03-22 21:31:40 +0700

    rebuild setup server, db & client

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
-const JWT_SECRET = process.env.JWT_SECRET
-// let JWT_SECRET = 'rahasia'
+// const JWT_SECRET = process.env.JWT_SECRET
+let JWT_SECRET = 'lk21asli_key'
 
 function sign(payload) {
     return jwt.sign(payload, JWT_SECRET)
