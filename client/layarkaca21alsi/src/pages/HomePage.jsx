f90b0e50bb35e44738ab4c0ee15516f119745071
abdulmajid <abdulmajid08180881@gmail.com>
2022-04-27 15:59:20 +0700

    fix style card movies in homePage

--- client/layarkaca21alsi/src/pages/HomePage.jsx
+++ client/layarkaca21alsi/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import { fetch_now_playing } from '../store/actions/actionMovies'
 import Loading from './Loading';
 import Error from './Error';
 import MovieCard from '../components/MovieCard';
+// import CardTemp from '../template/CardTemp';
 // import Slider from 'react-slick';
 // import "~slick-carousel/slick/slick.css"; 
 // import "~slick-carousel/slick/slick-theme.css";
@@ -63,34 +64,11 @@ function HomePage() {
         <span className=' border-b-2 border-yellow-700'></span>
         <h2 className=' lg:ml-20 text-5xl text-yellow-600'>Now Playing</h2>
       </div>
-      <div className="holder mx-auto w-10/12 grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-        {/* <Slider { ...settings }>
-        <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div> */}
-          {
-            now_playing?.map((movie, idx) => <MovieCard key={idx} movie={movie} />)
-          }
-        {/* </Slider> */}
-          {/* {
-              now_playing?.map((movie, idx) => <MovieCard key={idx} movie={movie} />)
-          } */}
-      </div>
+      <div className="holder mx-auto w-10/12 grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">        
+        {
+          now_playing?.map((movie, idx) => <MovieCard key={idx} movie={movie} />)
+        }        
+      </div>  
       <div className=' flex flex-row justify-center items-center'>
       <button className='prev' onClick={() => handlePrevPage()}>
         Previous
